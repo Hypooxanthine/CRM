96845fa4106d6f7affc3639eee41f41a612f3ab0
IsmailSALAH2000 <ismail.salah2000@yahoo.com>
2022-10-26 00:51:46 +0200

    Modification class Contact. Ajout de la doc pour la class Contact. Définition de la methode addInteraction

--- contact.cpp
+++ contact.cpp
@@ -1,19 +1,18 @@
 #include "contact.h"
+#include "date.h"
 
 Contact::     Contact(const std::string& id, const std::string& name,
                       const std::string& email,const std::string& phone,
-                      const std::string& photo, const unsigned cDate,
+                      const std::string& photoPath, const Date& date,
                       const std::vector<Interaction> interactionList)
 {
-
     setId(id);
     setName(name);
     setEmail(email);
     setPhone(phone);
-    setPhoto(photo);
-    setCreationDate(cDate);
+    setPhotoPath(photoPath);
+    setDate(date);
     setInteractionList(interactionList);
-
 }
 
 Contact::Contact(const Contact& c){
@@ -21,30 +20,15 @@ Contact::Contact(const Contact& c){
     setName(c.getName());
     setEmail(c.getEmail());
     setPhone(c.getPhone());
-    setPhoto(c.getPhoto());
-    setCreationDate(c.getCreationDate());
+    setPhotoPath(c.getPhotoPath());
+    setDate(c.getDate());
     setInteractionList(c.getInteractionList());
 }
 
-Contact::Contact(){}
-Contact::~Contact(){}
-
-
-void Contact::setId(const std::string& id){ this->id = id;}
-void Contact::setName(const std::string& name){this->Name = name;}
-void Contact::setEmail(const std::string& email){this->email = email;}
-void Contact::setPhone(const std::string& phone){this->phone = phone;}
-void Contact::setPhoto(const std::string& photo){this->photo = photo;}
-void Contact::setCreationDate(const unsigned& cDate){this->creationDate = cDate;}
-void Contact::setInteractionList(const std::vector<Interaction> &newInteractionList){this->interactionList =newInteractionList;}
-
-std::string Contact::getId() const {return this->id;}
-std::string Contact::getName() const {return this->Name;}
-std::string Contact::getEmail() const {return this->email;}
-std::string Contact::getPhone() const {return this->phone;}
-std::string Contact::getPhoto() const {return this->photo;}
-unsigned Contact::getCreationDate() const {return this->creationDate;}
-std::vector<Interaction> Contact::getInteractionList() const {return this->interactionList;}
+void Contact::addInteraction(Interaction interaction)
+{
+    Contact::interactionList.push_back(interaction);
+}
 
 
 std::ostream& operator<<(std::ostream& os, const Contact& c){
@@ -53,7 +37,7 @@ std::ostream& operator<<(std::ostream& os, const Contact& c){
         "\"Name\":\""<<c.getName()<<"\","<<
         "\"email\":\""<<c.getEmail()<<"\","<<
         "\"phone\":\""<<c.getPhone()<<"\","<<
-        "\"creationDate\":"<<c.getCreationDate()<<
+        "\"creationDate\":"<<c.getDate()<<
         "}";
     return os;
  }
--- contact.h
+++ contact.h
@@ -1,66 +1,147 @@
 #pragma once
 
-#include<string>
+#include <string>
 #include <iostream>
-#include<vector>
-#include<interaction.h>
+#include <vector>
+
+#include "interaction.h"
+#include "date.h"
 
 class Contact
 {
+public: //Publics methods
+    /**
+     * @brief Default constructor is deleted.
+     */
+    Contact() = delete;
+
+    /**
+     * @brief Has to be called for a known Contact.For example, when retrieving a Contact from the database, the Contact
+     * is known, so are the other parameters.
+     * @param id The id of the Contact.
+     * @param name The name of the Contact.
+     * @param email The email of the Contact.
+     * @param phone The phone of the Contact.
+     * @param photoPath The photo's path of the contact.
+     * @param date The date the Contact was created.
+     * @param interactionList The Contact's list of interactions.
+     */
+    Contact(const std::string& id, const std::string& name,
+            const std::string& email,const std::string& phone,
+            const std::string& photoPath, const Date& date,
+            const std::vector<Interaction> interactionList );
+
+    /**
+     * @brief Has to be called to copy a Contact.
+     * @param c The Contact to copy.
+     */
+    Contact(const Contact& c);
+
+    // Setters
+    /**
+     * @brief Sets the id of the Contact.
+     * @param The new id.
+     */
+    inline void setId(const std::string& id){ this->id = id;}
+
+    /**
+     * @brief Sets the Name of the Contact.
+     * @param The new name.
+     */
+    inline void setName(const std::string& name){this->Name = name;}
+
+    /**
+     * @brief Sets the email of the Contact.
+     * @param The new email.
+     */
+    inline void setEmail(const std::string& email){this->email = email;}
+
+    /**
+     * @brief Sets the phone of the Contact.
+     * @param The new phone.
+     */
+    inline void setPhone(const std::string& phone){this->phone = phone;}
+
+    /**
+     * @brief Sets the photo's path of the Contact.
+     * @param The new photo's path.
+     */
+    inline void setPhotoPath(const std::string& photoPath){this->photoPath = photoPath;}
+
+    /**
+     * @brief Sets the date of the Contact.
+     * @param The new date .
+     */
+    inline void setDate(const Date& date){this->date = date;}
+
+    /**
+     * @brief Sets the Contact's list of interactions.
+     * @param The new Contact's list of interactions.
+     */
+    inline void setInteractionList(const std::vector<Interaction>& InteractionList){this->interactionList = InteractionList;}
 
 
+    //Getters
 
-private:
+    /**
+     * @brief Gets the id of the Contact.
+     * @return The id of the Contact.
+     */
+    inline const std::string getId() const {return id;}
 
+    /**
+     * @brief Gets the Name of the Contact.
+     * @return The Name of the Contact.
+     */
+    inline const std::string getName() const {return Name;}
+
+    /**
+     * @brief Gets the email of the Contact.
+     * @return The email of the Contact.
+     */
+    inline const std::string getEmail() const {return email;}
+
+    /**
+     * @brief Gets the phone of the Contact.
+     * @return The phone of the contact.
+     */
+    inline const std::string getPhone() const {return phone;}
+
+    /**
+     * @brief Gets the photo's path of the Contact.
+     * @return The photo's path of the Contact.
+     */
+    inline const std::string getPhotoPath() const {return photoPath;}
+
+    /**
+     * @brief Gets the date of the Contact.
+     * @return The date of the Contact.
+     */
+    inline const Date getDate() const {return date;}
+
+    /**
+     * @brief Gets the Contact's list of interactions.
+     * @return The Contact's list of interactions.
+     */
+    inline const std::vector<Interaction> getInteractionList() const {return interactionList;}
+
+
+    /**
+     * @brief Add an interaction to the Contact's list of interactions.
+     * @param interaction to add to the list.
+     */
+    void addInteraction(Interaction interaction);
+
+    friend std::ostream& operator<<(std::ostream& os, const Contact& c);
+
+private: // Private memebers
     std::string id;
     std::string Name;
     std::string email;
     std::string phone;
-    std::string photo;
-    unsigned creationDate;
+    std::string photoPath;
+    Date date;
     std::vector<Interaction> interactionList;
-
-public:
-
-
-    Contact(const std::string& id, const std::string& name,
-            const std::string& email,const std::string& phone,
-            const std::string& photo, const unsigned cDate,
-            const std::vector<Interaction> interactionList );
-
-
-    Contact(const Contact& c);
-
-
-    Contact();
-    ~Contact();
-
-    //getters and setters
-    std::string getId() const;
-    void setId(const std::string &newId);
-
-    std::string getName() const;
-    void setName(const std::string &newName);
-
-    std::string getEmail() const;
-    void setEmail(const std::string &newEmail);
-
-    std::string getPhone() const;
-    void setPhone(const std::string &newPhone);
-
-    std::string getPhoto() const;
-    void setPhoto(const std::string &newPhoto);
-
-    unsigned getCreationDate() const;
-    void setCreationDate(const unsigned &newCreationDate);
-
-    std::vector<Interaction> getInteractionList() const;
-    void setInteractionList(const std::vector<Interaction> &newInteractionList );
-
-
-
-    friend std::ostream& operator<<(std::ostream& os, const Contact& c);
-
 };
 
 
